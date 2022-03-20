d4dd529c6283790c0a5ca7d63bd8e52db5def670
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-03-20 16:08:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,39 +13,10 @@ function getTrump() {
 function getYe() {
     fetch('https://api.kanye.rest/')
         .then(response => response.json())
-        .then(data => sessionStorage.setItem("ye", data.quote))
-        document.getElementById("ye").innerHTML=(sessionStorage.getItem("ye") ?? "Something is wrong...")
-
+        .then(data => document.getElementById("ye").innerHTML=(data.quote ?? "Something is wrong..."));
 
 }
 
 getYe()
 getRon()
 getTrump()
-
-/*function share(person, quote) {
-    text = ("Read this quote from "+person+"! "+quote+"\nFind more quotes at:")
-    try {
-        navigator.share("Quote",text, "https://zzz.jacobdrath.co/quotes/")
-    } catch(er) {
-        alert("Share is not supported. JS ERROR: "+er)
-    }
-
-}*/
-    var qu = 'Read this Quote from Kanye West: '+sessionStorage.getItem("ye")
-    const shareData = {
-        title: 'Quote',
-        text: qu,
-        url: 'https://zzz.jacobdrath.co/quotes/'
-      }
-    
-      const btn = document.querySelector('#YeShare');
-    
-      // Share must be triggered by "user activation"
-      btn.addEventListener('click', async () => {
-        try {
-          await navigator.share(shareData)
-        } catch(err) {
-          alert("Error: "+ err)
-        }
-      });
