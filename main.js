8f0787e46cd865ca765be0f21daffb9c40d8a63f
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-03-20 14:10:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function getRon() {
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+        .then(response => response.json())
+        .then(data => document.getElementById("ron").innerHTML=(data ?? "Something is wrong..."));
+
+}
+function getTrump() {
+    fetch('https://www.tronalddump.io/random/quote')
+        .then(response => response.json())
+        .then(data => document.getElementById("trump").innerHTML=(data.value ?? "Something is wrong..."));
+
+}
+function getYe() {
+    fetch('https://api.kanye.rest/')
+        .then(response => response.json())
+        .then(data => document.getElementById("ye").innerHTML=(data.quote ?? "Something is wrong..."));
+
+}
+
+getYe()
+getRon()
+getTrump()
